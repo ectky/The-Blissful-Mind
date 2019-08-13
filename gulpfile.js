6283f590009f0cd92677a46207d093fa8e466ec6
Elitsa <ectky3@gmail.com>
2019-08-14 02:07:32 +0300

    added sass files

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ let gulp = require("gulp"),
 	cp = require("child_process");
 
 gulp.task("css", function() {
-	return gulp.src( '_assets/css/**/*.css' )
+	return gulp.src( '_assets/scss/**/*.scss' )
 		.pipe( sass().on('error', sass.logError) )
 		.pipe( autoprefixer() )
 		.pipe( gulp.dest( './docs/css/' ) )
@@ -34,7 +34,7 @@ gulp.task("watch", function() {
 		}
 	});
 
-	gulp.watch( '_assets/css/**/*.css', gulp.series('css') );
+	gulp.watch( '_assets/scss/**/*.scss', gulp.series('css') );
 
 	gulp.watch(
 		[
